a8c42ec232355f3a70c3fe29e679e5680f3ad31b
fox <1750@qq.com>
2019-01-03 19:37:01 +0800

    rename file

--- src/pilo/core/threading/user_mutex.hpp
+++ src/pilo/core/threading/user_mutex.hpp
@@ -13,52 +13,52 @@ namespace pilo
                 user_mutex()
                 {
 #ifdef WINDOWS
-                    InitializeCriticalSection(&_internal_handle());
+                    InitializeCriticalSection(&internal_handle());
 #else
                     pthread_mutexattr_t attr;
                     pthread_mutexattr_init(&attr);
                     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-                    pthread_mutex_init(&_internal_handle(), &attr);
+                    pthread_mutex_init(&internal_handle(), &attr);
 #endif
                 }
 
                 ~user_mutex()
                 {
 #ifdef WINDOWS
-                    DeleteCriticalSection(&_internal_handle());
+                    DeleteCriticalSection(&internal_handle());
 #else
-                    pthread_mutex_destroy(&_internal_handle());
+                    pthread_mutex_destroy(&internal_handle());
 #endif
                 }
 
                 inline void lock() const
                 {
 #ifdef WINDOWS
-                    EnterCriticalSection((CRITICAL_SECTION*)&_internal_handle());
+                    EnterCriticalSection((CRITICAL_SECTION*)&internal_handle());
 #else
-                    pthread_mutex_lock((pthread_mutex_t*)&_internal_handle());
+                    pthread_mutex_lock((pthread_mutex_t*)&internal_handle());
 #endif
                 }
 
                 inline void unlock() const
                 {
 #ifdef WINDOWS
-                    LeaveCriticalSection((CRITICAL_SECTION*)&_internal_handle());
+                    LeaveCriticalSection((CRITICAL_SECTION*)&internal_handle());
 #else
-                    pthread_mutex_unlock((pthread_mutex_t*)&_internal_handle());
+                    pthread_mutex_unlock((pthread_mutex_t*)&internal_handle());
 #endif
                 }
 
                 inline bool trylock() const
                 {
 #ifdef WINDOWS
-                    if (TryEnterCriticalSection((CRITICAL_SECTION*)&_internal_handle()))
+                    if (TryEnterCriticalSection((CRITICAL_SECTION*)&internal_handle()))
                     {
                         return true;                           
                     }
 
 #else 
-                    if (0 == pthread_mutex_trylock((pthread_mutex_t*)&_internal_handle())) 
+                    if (0 == pthread_mutex_trylock((pthread_mutex_t*)&internal_handle())) 
                     {
                         return true;
                     }
@@ -67,12 +67,12 @@ namespace pilo
                 }
 
 #ifdef WINDOWS
-                CRITICAL_SECTION & _internal_handle() const
+                CRITICAL_SECTION & internal_handle() const
                 {
                     return _lock;
                 }
 #else
-                pthread_mutex_t & _internal_handle() const
+                pthread_mutex_t & internal_handle() const
                 {
                     return _lock;
                 }
