9511bbd0fe8e15c0bcb6911a17fad9a0a4ebc454
fox <1750@qq.com>
2019-01-03 19:33:45 +0800

    fox
    fix

---
+++ output/include/core/threading/user_mutex.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#include "core/coredefs.hpp"
+
+namespace pilo
+{
+    namespace core
+    {
+        namespace threading
+        {
+            class user_mutex
+            {
+            public:
+                user_mutex()
+                {
+#ifdef WINDOWS
+                    InitializeCriticalSection(&_internal_handle());
+#else
+                    pthread_mutexattr_t attr;
+                    pthread_mutexattr_init(&attr);
+                    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+                    pthread_mutex_init(&_internal_handle(), &attr);
+#endif
+                }
+
+                ~user_mutex()
+                {
+#ifdef WINDOWS
+                    DeleteCriticalSection(&_internal_handle());
+#else
+                    pthread_mutex_destroy(&_internal_handle());
+#endif
+                }
+
+                inline void lock() const
+                {
+#ifdef WINDOWS
+                    EnterCriticalSection((CRITICAL_SECTION*)&_internal_handle());
+#else
+                    pthread_mutex_lock((pthread_mutex_t*)&_internal_handle());
+#endif
+                }
+
+                inline void unlock() const
+                {
+#ifdef WINDOWS
+                    LeaveCriticalSection((CRITICAL_SECTION*)&_internal_handle());
+#else
+                    pthread_mutex_unlock((pthread_mutex_t*)&_internal_handle());
+#endif
+                }
+
+                inline bool trylock() const
+                {
+#ifdef WINDOWS
+                    if (TryEnterCriticalSection((CRITICAL_SECTION*)&_internal_handle()))
+                    {
+                        return true;                           
+                    }
+
+#else 
+                    if (0 == pthread_mutex_trylock((pthread_mutex_t*)&_internal_handle())) 
+                    {
+                        return true;
+                    }
+#endif
+                    return false;
+                }
+
+#ifdef WINDOWS
+                CRITICAL_SECTION & _internal_handle() const
+                {
+                    return _lock;
+                }
+#else
+                pthread_mutex_t & _internal_handle() const
+                {
+                    return _lock;
+                }
+#endif
+
+            protected:
+#ifdef WINDOWS
+                mutable CRITICAL_SECTION _lock;
+#else
+                mutable pthread_mutex_t _lock;
+#endif
+            };
+        }
+    }
+}
