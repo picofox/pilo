8f39ff04916cfbf22e6d8df5a29d1a03c1b9fc1b
picofox <17509294@qq.com>
2024-06-05 23:39:35 +0800

    path for linux

---
+++ src/pilo_stable_test/core/i18n/case_utf8_unicode.cpp
@@ -0,0 +1,24 @@
+﻿#include "../../pilo_stable_test.hpp"
+#include "pilo/core/testing/func_test_suite.hpp"
+#include "pilo/core/string/string_operation.hpp"
+#include "pilo/core/io/formatted_io.hpp"
+#include "pilo/core/testing/case_utils.hpp"
+#include "pilo/core/i18n/encoding_conversion.hpp"
+
+using namespace ::pilo::stable_test;
+using namespace ::pilo::core::testing;
+
+
+namespace pilo
+{
+	namespace stable_test
+	{
+		namespace core
+		{
+			namespace io
+			{
+
+			} //io
+		} // core
+	} // stable test
+} // pilo
